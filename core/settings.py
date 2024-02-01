b67dd9c2b3d0dd630a6a53f95a18b8e304bff30a
sumanbro0 <pandey.suman258@gmail.com>
2024-02-01 18:08:10 +0545

    d

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['greatcart-production.up.railway.app']
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://greatcart-production.up.railway.app/']
 
 # Quick-start development settings - unsuitable for production
