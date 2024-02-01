8c3987529e57da62b1ba5153143ac96de9d0f19a
sumanbro0 <pandey.suman258@gmail.com>
2024-02-01 18:06:21 +0545

    dd

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['greatcart-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://greatcart-production.up.railway.app/']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
